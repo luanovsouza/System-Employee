af07d38656fe224325e455df54aa182fead8593e
Luan Oliveira <luanvisouza037@gmail.com>
2025-08-12 17:50:05 -0300

    Add Constructor in Subclass OutSourcedEmployee and Method override Payment

--- ExercicioResolvido/Entities/OutSourcedEmployee.cs
+++ ExercicioResolvido/Entities/OutSourcedEmployee.cs
@@ -2,5 +2,24 @@
 
 public class OutSourcedEmployee : Employee
 {
-    
+    public double AdditionalCharge { get; set; }
+
+
+    public OutSourcedEmployee()
+    {
+    }
+
+    public OutSourcedEmployee(string? name, int hours, double valuePerHour, double additionalcharge) 
+        : base(name, hours, valuePerHour)
+    {
+        AdditionalCharge = additionalcharge;
+    }
+
+    public override double Payment()
+    {
+        double basepaymentet = base.Payment(); //Pegando o retorno do, calculo do pagamento do funcionário padrão 
+        double aditional = AdditionalCharge * 1.1; // Fazendo o calculo de 110%
+        
+        return basepaymentet + aditional; //Retornando a conta do salário mais o bonus
+    }
 }
